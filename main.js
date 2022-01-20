bea3cbfc4724020999ad6526aeedd30c38d8b9aa
Marina De Gaetani <marina.degaetani03@gmail.com>
2022-01-20 08:57:57 +0100

    ajout js + readme

--- main.js
+++ main.js
@@ -1,63 +1,99 @@
-// Toujours récupérer au debut du script les elements Html nécéssaires dans les constantes
+// Accès aux DOM
 const inputFrom = document.querySelector("#from");
 const inputTo = document.querySelector("#to");
 const switchButton = document.querySelector(".switch");
 const selectFromCurrencies = document.querySelector("#from-currencies");
 const selectToCurrencies = document.querySelector("#to-currencies");
-const submitInput = document.querySelector('[type="submit"]');
-const from = document.querySelector("form");
+const submitInput = document.querySelector("[type='submit']");
+const form = document.querySelector("form");
+
+// Déclaration des fonctions (DEBUT)
 
 const switchCurrencies = (event) => {
   event.preventDefault();
-  // Inverser les monnaies from et to
+
   console.log("le bouton switch a été cliqué");
+  [inputFrom.value, inputTo.value] = [inputTo.value, inputFrom.value];
+  [selectFromCurrencies.value, selectToCurrencies.value] = [selectToCurrencies.value, selectFromCurrencies.value];
+};
+/**
+ * Prend l'option sélectionnée d'un menu select et la désactive dans un deuxième menu select
+ * @param  {HTMLSelectElement} selectElement1 le menu select selectionné
+ * @param  {HTMLSelectElement} selectElement2 le menu select à modifier
+ */
+const disableSelectValue = (selectElement1, selectElement2) => {
+  const fromValue = selectElement1.value;
+  const nodes = selectElement2.querySelectorAll("option");
+
+  for (const node of nodes) {
+    node.removeAttribute("disabled");
+
+    if (node.value === fromValue) {
+      node.setAttribute("disabled", true);
+      node.style.color = "#27272750";
+    } else {
+      node.removeAttribute("disabled");
+    }
+  }
 };
 
 const submitForm = async (event) => {
-  // Empecher la page de se recharger après la soumission du formulaire
   event.preventDefault();
-  // Récupérer la valeur tapée dans le champ from
   const fromValue = inputFrom.value;
-  //Récupérer la valeur du select from
   const currencyFrom = selectFromCurrencies.value;
-  //Récupérer la valeur du select to
   const currencyTo = selectToCurrencies.value;
 
-  // Vérifier que from & to ne sont pas egaux
-  if (currencyFrom === currencyTo) {
-    alert("Vous avez mit les mêmes monnaies !");
-    return;
-  }
-  // Afficher une alerte si le champs from est vide
   if (!fromValue) {
-    alert("Vous n'avez pas entré de valeur!");
+    alert("Vous n'avez pas entré de valeur");
     return;
   }
-
-  console.log("fromValue", fromValue);
-  // Lancer la requête à l'API en lui fournissant la valeur tapée dans from
   try {
     const response = await fetch(
       `https://api.frankfurter.app/latest?amount=${fromValue}&from=${currencyFrom}&to=${currencyTo}`
     );
     const json = await response.json();
 
-    console.log("réponse complète :", json);
-
-    // differentes maniere d'affichage
-    //console.log("date :", json.date);
-    //console.log("date :", json["date"]);
-
-    console.log("résultat :", json.rates[currencyTo]);
-
-    // TODO : traitée la réponse de l'API en remplissant le champ to
+    inputTo.value = json.rates[currencyTo];
   } catch (error) {
     console.error("Erreur dans la requête:", error);
     alert("Oups ! Une erreur est arrivée, veuillez ré-essayer ultérieurement.");
   }
-  console.log("le formulaire à été validé");
 };
-switchButton.addEventListener("click", switchCurrencies);
-from.addEventListener("submit", submitForm);
 
-// AJAX = Asynchronously Javascript And XML
+
+
+const updateSelectValue = async () => {
+  try {
+    const result = await fetch(`https://api.frankfurter.app/currencies`);
+    const data = await result.json();
+
+    let htmlContent = "";
+    for (const [key, value] of Object.entries(data)) {
+      const option = `<option value="${key}">${value} (${key})</option>`;
+      htmlContent += option;
+    }
+
+    selectFromCurrencies.innerHTML = htmlContent;
+    selectToCurrencies.innerHTML = htmlContent;
+
+    disableSelectValue(selectFromCurrencies, selectToCurrencies);
+  } catch (error) {
+    console.error("Erreur updateSelectValue : ", error);
+  }
+};
+
+// Event handlers
+
+form.addEventListener("submit", submitForm);
+selectFromCurrencies.addEventListener("change", (event) => {
+  disableSelectValue(selectFromCurrencies, selectToCurrencies);
+});
+selectToCurrencies.addEventListener("change", (event) => {
+  disableSelectValue(selectToCurrencies, selectFromCurrencies);
+});
+
+switchButton.addEventListener("click", switchCurrencies);
+
+// Appel ddes fonctions au chargement de la page
+
+updateSelectValue();
